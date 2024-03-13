52fcc0052bafb39d93abfbd2f43309988058dd72
Stephanie <stephanieverissimopp@gmail.com>
2024-03-13 19:45:39 -0300

    include state management

--- app/src/main/java/com/stephanieverissimo/aluvery/screens/HomeScreen.kt
+++ app/src/main/java/com/stephanieverissimo/aluvery/screens/HomeScreen.kt
@@ -1,12 +1,19 @@
 package com.stephanieverissimo.aluvery.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,21 +24,26 @@ import com.stephanieverissimo.aluvery.ui.theme.AluveryTheme
 
 @Composable
 fun HomeScreen(sections: Map<String, List<Product>>) {
+    var text by remember {mutableStateOf("")}
     AluveryTheme {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-            LazyColumn(modifier = Modifier.fillMaxSize(),
-                       verticalArrangement = Arrangement.spacedBy(16.dp),
-                       contentPadding = PaddingValues(vertical = 16.dp)
-            ) {
-                
-                for (section in sections) {
-                    item() {
-                        val title = section.key
-                        val products = section.value
-                        ProductSection(title = title, products = products)
+            Column {
+                OutlinedTextField(value = text, onValueChange = { newValue ->
+                   text = newValue
+                })
+                LazyColumn(modifier = Modifier.fillMaxSize(),
+                           verticalArrangement = Arrangement.spacedBy(16.dp),
+                           contentPadding = PaddingValues(vertical = 16.dp)) {
+                    
+                    for (section in sections) {
+                        item() {
+                            val title = section.key
+                            val products = section.value
+                            ProductSection(title = title, products = products)
+                        }
                     }
+                    
                 }
-                
             }
             
         }
