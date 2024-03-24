4817c6013ee65b976d0a08b2504c2c084db594fa
Stephanie Pinheiro <stephanieverissimopp@gmail.com>
2024-03-24 15:28:14 -0300

    adjust product form

--- app/src/main/java/com/stephanieverissimo/aluvery/ui/activities/ProductsFormActivity.kt
+++ app/src/main/java/com/stephanieverissimo/aluvery/ui/activities/ProductsFormActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,16 +16,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
+import androidx.compose.ui.unit.sp
 import com.stephanieverissimo.aluvery.ui.theme.AluveryTheme
 
-class ProductsFormActivity: ComponentActivity() {
+class ProductsFormActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
+        setContent {
             AluveryTheme {
                 Surface {
-                   ProductsFormScreen()
+                    ProductsFormScreen()
                 }
             }
         }
@@ -31,33 +38,70 @@ class ProductsFormActivity: ComponentActivity() {
 
 @Composable
 fun ProductsFormScreen() {
-    Column {
-        Text(text = "Product Creation")
+    Column(
+        Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "Product Creation",
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            fontSize = 28.sp,
+        )
 
         var url by remember {
             mutableStateOf("")
         }
-        TextField(value = url, onValueChange = { url = it})
+        TextField(
+            value = url, onValueChange = { url = it },
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            label = { Text("Url")}
+        )
 
 
         var name by remember {
             mutableStateOf("")
         }
-        TextField(value = name, onValueChange = { name = it})
+        TextField(
+            value = name, onValueChange = { name = it },
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            label = { Text("Name")}
+        )
 
         var price by remember {
             mutableStateOf("")
         }
-        TextField(value = price, onValueChange = { price = it})
+        TextField(
+            value = price, onValueChange = { price = it },
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            label = { Text("Price")}
+        )
 
         var description by remember {
             mutableStateOf("")
         }
-        TextField(value = description, onValueChange = { description = it})
+        TextField(
+            value = description, onValueChange = { description = it },
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .heightIn(min = 100.dp),
+            label = { Text("Description")}
+        )
 
-        Button(onClick = {  }) {
+        Button(onClick = { },
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp)) {
             Text(text = "Save")
-            
+
         }
     }
 }
