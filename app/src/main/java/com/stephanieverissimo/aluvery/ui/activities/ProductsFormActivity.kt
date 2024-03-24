fff5b6f0b5e8f0e1eb4bba330504530f6cbbd83d
Stephanie Pinheiro <stephanieverissimopp@gmail.com>
2024-03-24 16:18:36 -0300

    some adjusts like padding and add a preview image when user include the url image

--- app/src/main/java/com/stephanieverissimo/aluvery/ui/activities/ProductsFormActivity.kt
+++ app/src/main/java/com/stephanieverissimo/aluvery/ui/activities/ProductsFormActivity.kt
@@ -1,13 +1,18 @@
 package com.stephanieverissimo.aluvery.ui.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,11 +23,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.min
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.stephanieverissimo.aluvery.R
+import com.stephanieverissimo.aluvery.models.Product
 import com.stephanieverissimo.aluvery.ui.theme.AluveryTheme
+import java.lang.NumberFormatException
+import java.math.BigDecimal
 
 class ProductsFormActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,9 +54,11 @@ fun ProductsFormScreen() {
     Column(
         Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
+        Spacer(modifier = Modifier)
         Text(
             text = "Product Creation",
             Modifier
@@ -55,11 +69,25 @@ fun ProductsFormScreen() {
         var url by remember {
             mutableStateOf("")
         }
+
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentDescription = "image of url",
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder),
+            )
+        }
+
         TextField(
             value = url, onValueChange = { url = it },
             Modifier
                 .fillMaxWidth(),
-            label = { Text("Url")}
+            label = { Text("Url") }
         )
 
 
@@ -70,7 +98,7 @@ fun ProductsFormScreen() {
             value = name, onValueChange = { name = it },
             Modifier
                 .fillMaxWidth(),
-            label = { Text("Name")}
+            label = { Text("Name") }
         )
 
         var price by remember {
@@ -80,7 +108,7 @@ fun ProductsFormScreen() {
             value = price, onValueChange = { price = it },
             Modifier
                 .fillMaxWidth(),
-            label = { Text("Price")}
+            label = { Text("Price") }
         )
 
         var description by remember {
@@ -91,15 +119,32 @@ fun ProductsFormScreen() {
             Modifier
                 .fillMaxWidth()
                 .heightIn(min = 100.dp),
-            label = { Text("Description")}
+            label = { Text("Description") }
         )
 
-        Button(onClick = { },
+        Button(
+            onClick = {
+                val converterBigDecimal = try {
+                    BigDecimal(price)
+                } catch (e: NumberFormatException) {
+                    BigDecimal.ZERO
+                }
+
+                val product = Product(
+                    name = name,
+                    price = converterBigDecimal,
+                    image = url,
+                    description = description,
+                )
+                Log.i("ProductForm", "ProductForm: $product")
+            },
             Modifier
-                .fillMaxWidth()) {
+                .fillMaxWidth()
+        ) {
             Text(text = "Save")
 
         }
+        Spacer(modifier = Modifier)
     }
 }
 
