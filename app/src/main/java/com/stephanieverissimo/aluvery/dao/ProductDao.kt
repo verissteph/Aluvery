4d0bb15eb6331ded4dae12685f8bde0a65d7a1b9
Stephanie Pinheiro <stephanieverissimopp@gmail.com>
2024-03-25 20:12:34 -0300

    create DAO of products

--- app/src/main/java/com/stephanieverissimo/aluvery/dao/ProductDao.kt
+++ app/src/main/java/com/stephanieverissimo/aluvery/dao/ProductDao.kt
@@ -1,4 +1,10 @@
 package com.stephanieverissimo.aluvery.dao
 
+import com.stephanieverissimo.aluvery.sampleData.sampleProducts
+
 class ProductDao {
+    companion object {
+        val products = sampleProducts.toMutableList()
+    }
+    fun products() = products.toList()
 }
