bcd9d7cb3b4a0ef5de9c0fa2cac3ba2f5a30a5ec
Stephanie Pinheiro <stephanieverissimopp@gmail.com>
2024-03-29 14:08:34 -0300

    replace mutableListOf to mutableStateOf

--- app/src/main/java/com/stephanieverissimo/aluvery/dao/ProductDao.kt
+++ app/src/main/java/com/stephanieverissimo/aluvery/dao/ProductDao.kt
@@ -3,18 +3,20 @@ package com.stephanieverissimo.aluvery.dao
 import androidx.compose.runtime.mutableStateListOf
 import com.stephanieverissimo.aluvery.models.Product
 import com.stephanieverissimo.aluvery.sampleData.sampleProducts
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 
 class ProductDao {
 
     companion object {
-        val products = mutableStateListOf<Product>()
+        val products = MutableStateFlow<List<Product>>(emptyList())
     }
 
-    fun products() = products.toList()
+    fun products() = products.asStateFlow()
 
     fun save(product: Product) {
-        products.add(product)
+        products.value = products().value + product
     }
 
 }
